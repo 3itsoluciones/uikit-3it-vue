3b040c7ee9bfe3bf9c6883449fb3529b5e2bb48b
fabian.fuentes <fabian.fuentes@MacBook-Air-de-fabianfuentes.local>
2024-09-03 17:20:12 -0400

    Configurando librería con prettier y eslint

--- vite.config.js
+++ vite.config.js
@@ -24,7 +24,7 @@ export default defineConfig({
   },
   build: {
     lib: {
-      entry: resolve(__dirname, 'src/index.js'),
+      entry: resolve(__dirname, 'src/index.ts'),
       name: 'uikit-3it-vue',
       fileName: 'uikit-3it-vue',
       formats: ['es', 'cjs', 'umd'],
