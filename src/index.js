0050473d8fd5385b91448c813d4d0487f59f2865
fabian.fuentes <fabian.fuentes@MacBook-Air-de-fabianfuentes.local>
2024-09-04 15:35:52 -0400

    Configurando librería con prettier y eslint

--- src/index.js
+++ src/index.js
@@ -1,28 +1,13 @@
 import * as components from './components'
-import './composables'
+import * as composables from './composables'
 import * as utils from './utils'
-//import './utils'
-/* import useValidator from './utils/useValidator.js'
-import useInputMask from './utils/useInputMask.js'
-import useKeypress from './utils/useKeypress.js'
-import useTable from './utils/useTable.js'
-import useFormat from './utils/useFormat'
-import useTooltip from './utils/useTooltip' */
 
 export default {
   install(app) {
     Object.keys(components).forEach((key) => {
       app.component(key, components[key])
     })
-  },
+  }
 }
 
- export {
-  utils
-/*   useValidator,
-  useInputMask,
-  useKeypress,
-  useTable,
-  useFormat,
-  useTooltip */
-}
+export { composables, utils }
