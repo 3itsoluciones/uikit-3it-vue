04dd817fa2e7a3180d706ddc3be59f17799b4872
fabian.fuentes <fabian.fuentes@MacBook-Air-de-fabianfuentes.local>
2024-09-04 09:28:43 -0400

    Configurando librería con prettier y eslint

--- public/@types/uikit-3it-vue.d.ts
+++ public/@types/uikit-3it-vue.d.ts
@@ -1,4 +1,18 @@
 declare module 'uikit-3it-vue' {
   const uikit: any
+  const useValidator: Function
+  const useInputMask: Function
+  const useTable: Function
+  const useFormat: Function
+  const useKeypress: Function
+  const useTooltip: Function
+  export {
+    useValidator,
+    useInputMask,
+    useKeypress,
+    useTable,
+    useFormat,
+    useTooltip
+  }
   export default uikit
 }
