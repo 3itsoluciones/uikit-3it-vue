11594ddc87e9a78ccc21306ce16a91eff11aa446
Fabián Fuentes <fabian.fuentes@3it.cl>
2024-11-07 15:19:20 -0300

    FIX: Cambio al componente breadcrumb para soportar redirect en las rutas

--- dist/uikit-3it-vue.js
+++ dist/uikit-3it-vue.js
@@ -72,7 +72,7 @@ const Kt = ["disabled"], Gt = /* @__PURE__ */ d("div", { class: "spinner-border 
     const t = $t(), o = N(() => {
       let n = [], a = "";
       return t.matched.forEach((i, c) => {
-        i.redirect ? a += "/" + i.redirect.split("/")[c + 1] : a += "/" + i.path.split("/")[c + 1], n.push({
+        a += "/" + i.path.split("/")[c + 1], n.push({
           name: i.name,
           title: i.meta.title || "Sin título",
           path: a,
