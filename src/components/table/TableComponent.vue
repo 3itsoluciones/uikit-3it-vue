d409df2ee9e6e40bb93c7b58a47ff9154e1b49e9
Fabián Fuentes <fabian.fuentes@3it.cl>
2024-11-11 17:21:56 -0300

    NEW: Modificaciones al componente table para soportar acciones dinamicas

--- src/components/table/TableComponent.vue
+++ src/components/table/TableComponent.vue
@@ -94,7 +94,7 @@ const handleCheckOthers = (value) => {
 }
 //Status
 const handleRecordStatus = (row) => {
-  return Object.keys(row).includes('active') ? (!row.active.status && 'disabled') : ''
+  return Object.keys(row).includes('status') ? (!row.status && 'disabled') : '' 
 }
 </script>
 
